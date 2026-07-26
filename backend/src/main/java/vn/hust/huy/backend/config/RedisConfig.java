b0cbfbd38de2d9c99f26546f604213551a122c68
huytd2004 <Huy.TD225859@sis.hust.edu.vn>
2026-07-26 17:18:03 +0700

    fix error redis

--- backend/src/main/java/vn/hust/huy/backend/config/RedisConfig.java
+++ backend/src/main/java/vn/hust/huy/backend/config/RedisConfig.java
@@ -3,9 +3,11 @@ package vn.hust.huy.backend.config;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -37,6 +39,13 @@ public class RedisConfig {
 
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfiguration(objectMapper))
+                .build();
     }
 
     @Bean
